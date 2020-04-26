d5b49f7fc3e1d0dcbc3c26b7ba19fa5f58e692bb
Nora <ncos@protonmail.ch>
2020-04-26 02:22:35 -0400

    Added rank formatting

--- src/main/scala/zone/nora/simplestats/commands/StatsCommand.scala
+++ src/main/scala/zone/nora/simplestats/commands/StatsCommand.scala
@@ -2,6 +2,7 @@ package zone.nora.simplestats.commands
 
 import java.util.UUID
 
+import com.google.gson.JsonObject
 import net.hypixel.api.HypixelAPI
 import net.hypixel.api.util.ILeveling
 import net.minecraft.command.{CommandBase, ICommandSender}
@@ -26,30 +27,37 @@ class StatsCommand extends CommandBase {
           Utils.breakline()
           return
         }
-        if (args.length == 0) {
-          Utils.error("/stats [player]")
-          return
+        args.length match {
+          case 0 => Utils.error("/stats [player]")
+          case 1 =>
+            val playerReply = new HypixelAPI(UUID.fromString(SimpleStats.apiKey)).getPlayerByName(args(0)).get()
+            if (!playerReply.isSuccess) {
+              Utils.error(s"Unexpected Error: ${playerReply.getCause}")
+              return
+            }
+            val player = playerReply.getPlayer
+            if (player == null) {
+              Utils.error("Invalid player.")
+              return
+            }
+            Utils.breakline()
+            firstLine(player)
+            printStat("Hypixel Level", try { ILeveling.getLevel(player.get("networkExp").getAsDouble)
+            } catch {case _: NullPointerException => "1" })
+            printStat("Achievement Points", player.get("achievementPoints"))
+            printStat("Karma", player.get("karma"))
+            printStat("Discord", try {
+              player.get("socialMedia").getAsJsonObject.get("links").getAsJsonObject.get("DISCORD").getAsString
+            } catch { case _: NullPointerException => "N/A" })
+            printStat("First Login", Utils.parseTime(player.get("firstLogin").getAsLong))
+            printStat("Last Login", try { Utils.parseTime(player.get("lastLogin").getAsLong)
+            } catch { case _: NullPointerException => "Hidden" })
+            printStat("Online", try { player.get("lastLogin").getAsLong > player.get("lastLogout").getAsLong
+            } catch { case _: NullPointerException => false })
+            Utils.breakline()
+          case _ =>
+
         }
-        val playerReply = new HypixelAPI(UUID.fromString(SimpleStats.apiKey)).getPlayerByName(args(0)).get()
-        if (!playerReply.isSuccess) {
-          Utils.error(s"Unexpected Error: ${playerReply.getCause}")
-          return
-        }
-        val player = playerReply.getPlayer
-        Utils.breakline()
-        Utils.put(s"Stats of ${player.get("displayname").getAsString}")
-        printStat("Hypixel Level", ILeveling.getLevel(player.get("networkExp").getAsDouble))
-        printStat("Achievement Points", player.get("achievementPoints"))
-        printStat("Karma", player.get("karma"))
-        printStat("Discord", try {
-          player.get("socialMedia").getAsJsonObject.get("links").getAsJsonObject.get("DISCORD").getAsString
-        } catch { case _: NullPointerException => "N/A" })
-        printStat("First Login", Utils.parseTime(player.get("firstLogin").getAsLong))
-        printStat("Last Login", Utils.parseTime(player.get("lastLogin").getAsLong))
-        printStat("Online", try {
-          player.get("lastLogin").getAsLong > player.get("lastLogout").getAsLong
-        } catch { case _: NullPointerException => false })
-        Utils.breakline()
       }
     })
     thread.start()
@@ -58,6 +66,12 @@ class StatsCommand extends CommandBase {
   override def canCommandSenderUseCommand(sender: ICommandSender): Boolean = true
 
   override def isUsernameIndex(args: Array[String], index: Int): Boolean = true
+
+  def firstLine(player: JsonObject, game: String = ""): Unit = {
+    val s = if (game == "") "S" else s"$game s"
+    val rank = try { Utils.getRank(player) } catch { case _: Exception => "" }
+    Utils.put(s"${s}tats of ${if (rank.endsWith("]")) s"$rank " else rank}${player.get("displayname").getAsString}")
+  }
 
   def printStat(name: String, value: Any): Unit = Utils.put(s"$name: ${if (value == null) "N/A" else value}")
 }
--- src/main/scala/zone/nora/simplestats/util/Utils.scala
+++ src/main/scala/zone/nora/simplestats/util/Utils.scala
@@ -4,6 +4,7 @@ import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.util.{Date, UUID}
 
+import com.google.gson.JsonObject
 import net.hypixel.api.HypixelAPI
 import net.minecraft.client.Minecraft
 import net.minecraft.util.ChatComponentText
@@ -18,6 +19,55 @@ object Utils {
     case _: Exception => false
   }
 
+  def getRank(player: JsonObject): String = {
+    if (player.has("prefix")) return player.get("prefix").getAsString
+
+    val rank = if (player.has("rank")) player.get("rank").getAsString else null
+    var playerRank = ""
+    playerRank = if (rank != null && rank != "NORMAL") rank
+    else if (player.has("newPackageRank")) player.get("newPackageRank").getAsString
+    else if (player.has("packageRank")) player.get("packageRank").getAsString
+    else null
+    if (playerRank == "MVP_PLUS" && player.get("monthlyPackageRank").getAsString == "SUPERSTAR")
+      playerRank = "MVP_PLUS_PLUS"
+    if (rank == "NONE") playerRank = null
+    val plusColour = if (player.has("rankPlusColor"))
+      colourNameToCode(player.get("rankPlusColor").getAsString)
+    val plusPlusColour = if (player.has("monthlyRankColor"))
+      colourNameToCode(player.get("monthlyRankColor").getAsString)
+    playerRank match {
+      case "VIP" => "\u00a7a[VIP]"
+      case "VIP_PLUS" => "\u00a7a[VIP\u00a76+\u00a7a]"
+      case "MVP" => "\u00a7b[MVP]"
+      case "MVP_PLUS" => s"\u00a7b[MVP$plusColour+\u00a7b]"
+      case "MVP_PLUS_PLUS" => s"$plusPlusColour[MVP$plusColour++$plusPlusColour]"
+      case "HELPER" => "\u00a79[HELPER]"
+      case "MODERATOR" => "\u00a72[MOD]"
+      case "ADMIN" => "\u00a7c[ADMIN]"
+      case "YOUTUBER" => "\u00a7c[\u00a7fYOUTUBER\u00a7c]"
+      case null => "\u00a77"
+      case _ => "\u00a77"
+    }
+  }
+
+  def colourNameToCode(colour: String): String = colour.toLowerCase() match {
+    case "black" => "\u00a70"
+    case "dark_green" => "\u00a72"
+    case "dark_aqua" => "\u00a73"
+    case "dark_red" => "\u00a74"
+    case "dark_purple" => "\u00a75"
+    case "gold" => "\u00a76"
+    case "gray" => "\u00a77"
+    case "dark_gray" => "\u00a78"
+    case "blue" => "\u00a79"
+    case "green" => "\u00a7a"
+    case "aqua" => "\u00a7b"
+    case "red" => "\u00a7c"
+    case "light_purple" => "\u00a7d"
+    case "yellow" => "\u00a7e"
+    case "white" => "\u00a7f"
+  }
+
   def parseTime(time: Long): String = try
     new SimpleDateFormat("dd/MM/yyyy").format(new Date(new Timestamp(time).getTime))
   catch { case _: Exception => "N/A" } // Hypixel Staff can hide their stats, which causes this function to freak out.
@@ -28,8 +78,8 @@ object Utils {
 
   def breakline(): Unit = {
     val dashes = new StringBuilder
-    val numberOfDashes = Math.floor((280 * mc.gameSettings.chatWidth + 40) / 320 * (1 / mc.gameSettings.chatScale) * 53).toInt
-    for (_ <- 1 to numberOfDashes) dashes.append("-")
+    val dash = Math.floor((280 * mc.gameSettings.chatWidth + 40) / 320 * (1 / mc.gameSettings.chatScale) * 53).toInt
+    for (_ <- 1 to dash) dashes.append("-")
     mc.thePlayer.addChatMessage(new ChatComponentText(s"\u00a79\u00a7m$dashes"))
   }
 }
